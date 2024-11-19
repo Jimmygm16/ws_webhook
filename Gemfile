f2e6a386dd89e674b0d5b4f3d5ea2f8a57e2e110
jimmygm16 <jimmy@kommit.co>
2024-11-19 11:02:46 -0500

    Initial webhook implementation

--- Gemfile
+++ Gemfile
@@ -4,7 +4,11 @@ source "https://rubygems.org"
 
 # gem "rails"
 
+gem "bas", path: "../bas"
+
 gem "sinatra", "~> 4.1"
 
 gem "rackup", "~> 2.2"
 gem "puma", "~> 6.4"
+
+gem "dotenv", "~> 3.1"
--- app.rb
+++ app.rb
@@ -1,5 +1,54 @@
-require 'sinatra'
+# frozen_string_literal: true
 
-get '/hello' do
-  'Hello, world!'
+#require "bas/shared_storage/postgres"
+#require_relative "../../formatters/whatsapp_formatter"
+require 'bas'
+require "sinatra"
+require "json"
+require "dotenv/load"
+# require_relative '../lib/fetch_instructions'
+
+set :server, :puma
+TOKEN = ENV.fetch("TOKEN")
+
+get "/webhook" do
+  challenge = params["hub.challenge"]
+  verification_token = params["hub.verify_token"]
+  if verification_token == TOKEN
+    status 200
+    body challenge
+  else
+    status 403
+  end
+end
+
+post "/webhook" do
+  request_body = request.body.read
+  data = JSON.parse(request_body)
+  connection = {
+    host: ENV.fetch("DB_HOST"),
+    port: ENV.fetch("DB_PORT"),
+    dbname: ENV.fetch("POSTGRES_DB"),
+    user: ENV.fetch("POSTGRES_USER"),
+    password: ENV.fetch("POSTGRES_PASSWORD")
+  }
+  write_options = {
+    connection: connection,
+    db_table: "api_data",
+    tag: "FetchFromAPI"
+  }
+
+  # fetch_instructions = Bots::FetchInstructions.new(data, connection)
+  begin
+    #base_formatter = Formatters::WhatsAppFormatter.new
+    #data = base_formatter.process(data)
+    shared_storage = Bas::SharedStorage::Postgres.new(write_options: write_options)
+    shared_storage.write(success: data)
+  rescue => e
+    logger.error "Failed to process message: #{e.message}"
+    status 500
+    body "Internal Server Error"
+  end
+
+  status 200
 end
